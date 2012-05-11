868032aac436270198444c1622dcb7d470dc6c95
robertsantz <robertsantz@gmail.com>
2012-05-11 12:49:29 +0700

    Add config without database.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Wgs::Application.config.secret_token = 'f9e15ea6f0ba166fff1bceb11224d363abcd34608082d4d77ff6bc335796de10b5b34c093250bee550331ebcbb8ae22303db15fdc84682ebf742ab229088042a'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Wgs::Application.config.session_store :cookie_store, key: '_wgs_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Wgs::Application.config.session_store :active_record_store
